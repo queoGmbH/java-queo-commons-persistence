8bd1853dbe7f3fd6d67cab008183484ff4f073fe
Ralph Engelmann <r.engelmann@queo-group.com>
2014-05-16 06:52:25 +0000

    Die Idee war ja gerade die NotFoundRuntimeException zu werfen

--- queo-commons-persistence-extra-nullaware/src/main/java/com/queomedia/persistence/extra/nullaware/NullawareNameControlledJpaRepositoryFactoryBean.java
+++ queo-commons-persistence-extra-nullaware/src/main/java/com/queomedia/persistence/extra/nullaware/NullawareNameControlledJpaRepositoryFactoryBean.java
@@ -3,14 +3,21 @@ package com.queomedia.persistence.extra.nullaware;
 import java.io.Serializable;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.support.JpaRepositoryFactoryBean;
+
+import com.queomedia.commons.exceptions.NotFoundRuntimeException;
 
 import de.humanfork.spring.data.jpa.nullaware.NullawareJpaRepositoryFactoryBean;
 import de.humanfork.spring.data.jpa.nullaware.controll.NameControlledNullResultActionFactory;
 
+/**
+ * A Spring-Data-Jpa {@link JpaRepositoryFactoryBean} that apply the Nullware support configured to throw a
+ * {@link NotFoundRuntimeException} by default.
+ */
 public class NullawareNameControlledJpaRepositoryFactoryBean<T extends JpaRepository<Object, Serializable>> extends
         NullawareJpaRepositoryFactoryBean<T> {
 
-    private static final NameControlledNullResultActionFactory nameControlledNullResultActionFactory = new NameControlledNullResultActionFactory(RuntimeException.class);
+    private static final NameControlledNullResultActionFactory nameControlledNullResultActionFactory = new NameControlledNullResultActionFactory(NotFoundRuntimeException.class);
 
     public NullawareNameControlledJpaRepositoryFactoryBean() {
         super(nameControlledNullResultActionFactory);
