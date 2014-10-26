4f846ae39a88d5a10fd85992dc1a873ef9391299
Ralph Engelmann <r.engelmann@queo-group.com>
2014-10-26 16:19:11 +0000

    RESOLVED - issue RUM-149: AbstractGenericDaoFake nach queo-commons-persistence-test auslagern https://jira.queo-group.com/browse/RUM-149

---
+++ queo-commons-persistence-extra-fake/src/main/java/com/queomedia/persistence/extra/fake/AbstractGenericDaoFake.java
@@ -0,0 +1,250 @@
+package com.queomedia.persistence.extra.fake;
+
+import java.lang.reflect.Field;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import org.apache.commons.beanutils.BeanComparator;
+import org.apache.commons.collections.comparators.ReverseComparator;
+import org.apache.commons.lang.NotImplementedException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.domain.Sort.Order;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.util.ReflectionUtils;
+
+import com.queomedia.commons.checks.Check;
+import com.queomedia.commons.exceptions.NotFoundRuntimeException;
+import com.queomedia.persistence.BusinessEntity;
+import com.queomedia.persistence.BusinessId;
+
+/**
+ * In memory DAO Fake implementation, usefull for tests.
+ * 
+ * @author Ralph Engelmann
+ *
+ * @param <T> the entity type
+ */
+public class AbstractGenericDaoFake<T extends BusinessEntity<T>> implements JpaRepository<T, Long> {
+
+    private long idCounter = 0;
+
+    private final Set<T> savedEntities;
+
+    public AbstractGenericDaoFake(final T... alreadyExistingTs) {
+        this.savedEntities = new HashSet<T>();
+
+        this.save(Arrays.asList(alreadyExistingTs));
+    }
+
+    public Long getId(final T entity) {
+        return entity.getId();
+    }
+
+    public boolean addToSavedEntities(final T entity) {
+        return this.savedEntities.add(entity);
+    }
+
+    public boolean addAllToSavedEntities(final T... entities) {
+        return this.savedEntities.addAll(Arrays.asList(entities));
+    }
+
+    public boolean removeFromSavedEntities(final T entity) {
+        return this.savedEntities.remove(entity);
+    }
+
+    public long incAndGetIdCounter() {
+        this.idCounter++;
+        return this.idCounter;
+    }
+
+    Set<T> getSavedEntities() {
+        return Collections.unmodifiableSet(this.savedEntities);
+    }
+
+    @Override
+    public <S extends T> S save(S entity) {
+        setFieldByReflection(entity, "id", this.incAndGetIdCounter());
+
+        this.addToSavedEntities(entity);
+        return entity;
+    }
+
+    @Override
+    public <S extends T> List<S> save(Iterable<S> entities) {
+        ArrayList<S> result = new ArrayList<S>();
+
+        for (S entity : entities) {
+            result.add(this.save(entity));
+        }
+
+        return result;
+    }
+
+    @Override
+    public <S extends T> S saveAndFlush(S entity) {
+        return this.save(entity);
+    }
+
+    @Override
+    public T getOne(final Long primaryKey) {
+        T one = this.findOne(primaryKey);
+        if (one != null) {
+            return one;
+        } else {
+            throw new RuntimeException("not found");
+        }
+    }
+
+    @Override
+    public boolean exists(final Long primaryKey) {
+        return this.findOne(primaryKey) != null;
+    }
+
+    @Override
+    public List<T> findAll() {
+        return new ArrayList<T>(this.getSavedEntities());
+    }
+
+    //Suppress unchecked is ok, because the BeanComparator works on every type
+    @SuppressWarnings("unchecked")
+    protected Comparator<T> buildComparatorForSorting(Sort sort) {
+        Order firstOrder = sort.iterator().next();
+
+        BeanComparator beanComparator = new BeanComparator(firstOrder.getProperty());
+        if (firstOrder.isAscending()) {
+            return beanComparator;
+        } else {
+            return new ReverseComparator(beanComparator);
+        }
+    }
+
+    @Override
+    public List<T> findAll(final Sort sort) {
+        List<T> list = this.findAll();
+        Collections.sort(list, buildComparatorForSorting(sort));
+        return list;
+    }
+
+    @Override
+    public Page<T> findAll(final Pageable pageable) {
+        throw new NotImplementedException();
+    }
+
+    @Override
+    public long count() {
+        return this.getSavedEntities().size();
+    }
+
+    @Override
+    public void delete(final T entity) {
+        this.removeFromSavedEntities(entity);
+
+    }
+
+    @Override
+    public void delete(Iterable<? extends T> entities) {
+        for (T entity : entities) {
+            this.delete(entity);
+        }
+    }
+
+    @Override
+    public void deleteAll() {
+        this.delete(this.findAll());
+    }
+
+    @Override
+    public void flush() {
+        //nothing to do        
+    }
+
+    public T getByBusinessId(final BusinessId<T> businessId) throws NotFoundRuntimeException {
+        T entity = findByBusinessId(businessId);
+        if (entity != null) {
+            return entity;
+        } else {
+            throw new NotFoundRuntimeException();
+        }
+    }
+
+    public T findByBusinessId(final BusinessId<T> businessId) {
+        for (T entitiy : this.savedEntities) {
+            if (entitiy.getBusinessId().equals(businessId)) {
+                return entitiy;
+            }
+        }
+        return null;
+    }
+
+    @Override
+    public T findOne(Long primaryKey) {
+        for (T entity : this.getSavedEntities()) {
+            if (getId(entity).equals(primaryKey)) {
+                return entity;
+            }
+        }
+        return null;
+    }
+
+    @Override
+    public void delete(Long id) {
+        this.delete(this.getOne(id));
+    }
+
+    @Override
+    public List<T> findAll(Iterable<Long> primaryKeys) {
+        List<T> result = new ArrayList<T>();
+
+        for (Long primaryKey : primaryKeys) {
+            T found = findOne(primaryKey);
+            if (found != null) {
+                result.add(found);
+            }
+        }
+        return result;
+    }
+
+    @Override
+    public void deleteInBatch(Iterable<T> entities) {
+        this.delete(entities);
+    }
+
+    @Override
+    public void deleteAllInBatch() {
+        this.deleteAll();
+    }
+
+    /**
+     * Set the {@link Field field} with the given {@code name} on the provided
+     * {@link Object target object} to the supplied {@code value}.
+     *
+     *
+     * @param target the target object on which to set the field
+     * @param name the name of the field to set
+     * @param value the value to set
+     * @see ReflectionUtils#findField(Class, String, Class)
+     * @see ReflectionUtils#makeAccessible(Field)
+     * @see ReflectionUtils#setField(Field, Object, Object)
+     */
+    private static void setFieldByReflection(Object target, String name, Object value) {
+        Check.notNullArgument(target, "target");
+        Check.notNullArgument(name, "name");
+        //value can be null of course    
+
+        Field field = ReflectionUtils.findField(target.getClass(), name);
+
+        if (field == null) {
+            throw new IllegalArgumentException(String.format("Could not find field [%s] on target [%s]", name, target));
+        }
+
+        ReflectionUtils.makeAccessible(field);
+        ReflectionUtils.setField(field, target, value);
+    }
+}
