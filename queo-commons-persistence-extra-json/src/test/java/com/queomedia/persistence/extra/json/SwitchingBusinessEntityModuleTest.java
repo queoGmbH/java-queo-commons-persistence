56ffd497cfe342b28798df884bb073b4fb3b7e97
Ralph Engelmann <r.engelmann@queo-group.com>
2020-03-20 16:24:03 +0000

    Tests

--- queo-commons-persistence-extra-json/src/test/java/com/queomedia/persistence/extra/json/SwitchingBusinessEntityModuleTest.java
+++ queo-commons-persistence-extra-json/src/test/java/com/queomedia/persistence/extra/json/SwitchingBusinessEntityModuleTest.java
@@ -4,20 +4,25 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.Assert.assertEquals;
 
 import java.io.IOException;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.jmock.Expectations;
 import org.jmock.integration.junit4.JUnitRuleMockery;
 import org.jmock.lib.legacy.ClassImposteriser;
-import org.json.JSONException;
 import org.junit.Rule;
 import org.junit.Test;
 import org.skyscreamer.jsonassert.JSONAssert;
 import org.skyscreamer.jsonassert.JSONCompareMode;
+import org.springframework.core.ConfigurableObjectInputStream;
 
 import com.fasterxml.jackson.core.JsonParseException;
-import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.JavaType;
 import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.queomedia.commons.checks.Check;
 import com.queomedia.persistence.BusinessEntity;
 import com.queomedia.persistence.BusinessId;
 import com.queomedia.persistence.GeneralLoaderDao;
@@ -78,6 +83,33 @@ public class SwitchingBusinessEntityModuleTest {
 
     public ObjectMapper configuredObjectMapper() {
         return configuredObjectMapper(this.context.mock(GeneralLoaderDao.class));
+    }
+
+    /**
+     * Build a Jackson Object Mappwer with {@link SwitchingBusinessEntityModule}
+     * that use a {@link GeneralLoaderDao}-mock that expect exactly one invocation
+     * of {@link GeneralLoaderDao#getByBusinessId(BusinessId, Class)} for the given
+     * {@code entity}.
+     * 
+     * @param entity the entity that is returned by the
+     *               {@link GeneralLoaderDao}-mock
+     * @return a Jackson Object mapper with {@link SwitchingBusinessEntityModule}
+     *         and mocked {@link GeneralLoaderDao}
+     */
+    @SuppressWarnings("unchecked")
+    public <T extends BusinessEntity<? extends Serializable>> ObjectMapper configuredObjectMapper(
+            final BusinessEntity<T> entity) {
+        Check.notNullArgument(entity, "entity");
+
+        GeneralLoaderDao generalLoaderDao = this.context.mock(GeneralLoaderDao.class);
+        this.context.checking(new Expectations() {
+            {
+                oneOf(generalLoaderDao).getByBusinessId(entity.getBusinessId(), (Class<T>) entity.getClass());
+                will(returnValue(entity));
+            }
+        });
+
+        return configuredObjectMapper(generalLoaderDao);
     }
 
     @SwitchingBusinessEntityAnnotation(BusinessEntitySerialization.BUSINESS_ID)
@@ -141,6 +173,70 @@ public class SwitchingBusinessEntityModuleTest {
         public String getValue2() {
             return value2;
         }
+    }
+
+    public static class ListContainer {
+        private List<DemoBusinessEntity> list;
+
+        public ListContainer() {
+            super();
+        }
+
+        public ListContainer(List<DemoBusinessEntity> list) {
+            this.list = new ArrayList<>(list);
+        }
+
+        public List<DemoBusinessEntity> getList() {
+            return list;
+        }
+
+        public void setList(List<DemoBusinessEntity> list) {
+            this.list = list;
+        }
+    }
+
+    @SwitchingBusinessEntityAnnotation(BusinessEntitySerialization.ENTITY)
+    public static class GenericEntityWrapper<T> {
+        private T content;
+
+        public GenericEntityWrapper() {
+            super();
+        }
+
+        public GenericEntityWrapper(T content) {
+            this.content = content;
+        }
+
+        public T getContent() {
+            return content;
+        }
+
+        public void setContent(T content) {
+            this.content = content;
+        }
+
+    }
+
+    @SwitchingBusinessEntityAnnotation(BusinessEntitySerialization.BUSINESS_ID)
+    public static class GenericBusinessIdWrapper<T> {
+        private T content;
+
+        public GenericBusinessIdWrapper() {
+            super();
+        }
+
+        public GenericBusinessIdWrapper(T content) {
+            this.content = content;
+        }
+
+        public T getContent() {
+            return content;
+        }
+
+        public void setContent(T content) {
+            this.content = content;
+        }
+
     }
 
     /**
@@ -299,4 +395,57 @@ public class SwitchingBusinessEntityModuleTest {
         assertThat(result).usingRecursiveComparison().isEqualTo(
                 new DemoContainerClassEntity("v1", new DemoBusinessEntity(new BusinessId<>(123), "Hello World"), "v2"));
     }
+
+    @Test
+    public void testSerializeDemoBusinessEntityDemo_withEntityWrapper() throws Exception {
+        DemoBusinessEntity content = new DemoBusinessEntity(new BusinessId<>(123), "Hello World");
+        GenericEntityWrapper<DemoBusinessEntity> container = new GenericEntityWrapper<>(content);
+
+        String jsonResult = configuredObjectMapper().writeValueAsString(container);
+
+        JSONAssert.assertEquals("{'content':{'businessId':'123', 'content':'Hello World'}}", jsonResult,
+                JSONCompareMode.NON_EXTENSIBLE);
+    }
+
+    @Test
+    public void testDeserializeDemoBusinessEntityDemo_withEntityWrapper() throws Exception {
+
+        final String jsonString = "{'content':{'businessId':'123', 'content':'Hello World'}}".replace("'", "\"");
+        ObjectMapper jacksonMapper = configuredObjectMapper();
+
+        GenericEntityWrapper<DemoBusinessEntity> result = jacksonMapper.readValue(jsonString,
+                new TypeReference<GenericEntityWrapper<DemoBusinessEntity>>() {
+                });
+
+        DemoBusinessEntity expectedContent = new DemoBusinessEntity(new BusinessId<>(123), "Hello World");
+        GenericEntityWrapper<DemoBusinessEntity> expectedContainer = new GenericEntityWrapper<>(expectedContent);
+        assertThat(result).usingRecursiveComparison().isEqualTo(expectedContainer);
+    }
+
+    @Test
+    public void testSerializeDemoBusinessEntityDemo_withBidWrapper() throws Exception {
+        DemoBusinessEntity content = new DemoBusinessEntity(new BusinessId<>(123), "Hello World");
+        GenericBusinessIdWrapper<DemoBusinessEntity> container = new GenericBusinessIdWrapper<>(content);
+
+        String jsonResult = configuredObjectMapper().writeValueAsString(container);
+
+        JSONAssert.assertEquals("{'content':'123'}", jsonResult, JSONCompareMode.NON_EXTENSIBLE);
+    }
+
+    @Test
+    public void testDeserializeDemoBusinessEntityDemo_withBidWrapper() throws Exception {
+
+        final String jsonString = "{'content':'123'}".replace("'", "\"");
+
+        DemoBusinessEntity businessEntity = new DemoBusinessEntity(new BusinessId<>(123), "Hello World");
+        ObjectMapper jacksonMapper = configuredObjectMapper(businessEntity);
+        GenericBusinessIdWrapper<DemoBusinessEntity> result = jacksonMapper.readValue(jsonString,
+                new TypeReference<GenericBusinessIdWrapper<DemoBusinessEntity>>() {
+                });
+       
+        
+        GenericBusinessIdWrapper<DemoBusinessEntity> expectedContainer = new GenericBusinessIdWrapper<>(businessEntity);
+        assertThat(result).usingRecursiveComparison().isEqualTo(expectedContainer);
+    }
+
 }
