ce36715fdf1329c86cde9c7576a37ce68fa05b57
Ralph Engelmann <r.engelmann@queo-group.com>
2020-03-27 09:41:06 +0000

    Javadoc

--- queo-commons-persistence-extra-json/src/main/java/com/queomedia/persistence/extra/json/switchingbusinessentity/SwitchingAnnotationScanner.java
+++ queo-commons-persistence-extra-json/src/main/java/com/queomedia/persistence/extra/json/switchingbusinessentity/SwitchingAnnotationScanner.java
@@ -1,20 +1,19 @@
 package com.queomedia.persistence.extra.json.switchingbusinessentity;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-
 import java.lang.reflect.Field;
 import java.lang.reflect.Method;
 import java.util.Locale;
 import java.util.Optional;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import com.fasterxml.jackson.core.JsonStreamContext;
 import com.queomedia.commons.checks.Check;
 
 /**
  * Scan the json-context-path to find a class that is annotated with {@link SwitchingBusinessEntityAnnotation}.
- * 
+ *
  * <p>
  * This scanner scan for the annotation (in this order) at the field that match the json-name,
  * the getter that match the field name and then at the class. If no annotation is found, then the scanner check
@@ -22,10 +21,10 @@ import com.queomedia.commons.checks.Check;
  * </p>
  */
 public class SwitchingAnnotationScanner {
-    
+
     /** Logger for this class. */
     private static final Logger LOGGER = LoggerFactory.getLogger(SwitchingAnnotationScanner.class);
-    
+
     /** The serialization mode returned if no annotation is found. */
     private final BusinessEntitySerializationMode defaultMode;
 
@@ -40,15 +39,27 @@ public class SwitchingAnnotationScanner {
         this.defaultMode = defaultMode;
     }
 
+    /**
+     * Get the {@link BusinessEntitySerializationMode} for the current object of the given Json-Context.
+     *
+     * @param context the Json-Context
+     * @return the switch definition - with fallback to {@link #defaultMode} if no annotation is found
+     */
     BusinessEntitySerializationMode getSwitchDefinition(final JsonStreamContext context) {
         Check.notNullArgument(context, "context");
-        
+
         return findSwitchDefinition(context).orElse(this.defaultMode);
     }
 
+    /**
+     * Get the {@link BusinessEntitySerializationMode} for the current object of the given Json-Context without fallback to default.
+     *
+     * @param context the Json-Context
+     * @return the switch definition, or empty if no annotation is found
+     */
     Optional<BusinessEntitySerializationMode> findSwitchDefinition(final JsonStreamContext context) {
         Check.notNullArgument(context, "context");
-        
+
         return findSwitchingBusinessEntityAnnotation(context).map(SwitchingBusinessEntityAnnotation::value);
     }
 
@@ -56,19 +67,22 @@ public class SwitchingAnnotationScanner {
      * Try to find the {@link SwitchingBusinessEntityAnnotation} in the given json {@code context}s
      * {@link JsonStreamContext#getCurrentValue() context.getCurrentValue()} or if not found recursive in the
      * {@link JsonStreamContext#getParent() context.getParent()}.
+     * 
+     * <p>
+     * Properties form Superclass are also checked: in this order:
+     * </p>
      *
      * @param context the {@link JsonStreamContext} thats currentValue and parentContext is scanned
      * @return the first found {@link SwitchingBusinessEntityAnnotation}
      */
-    Optional<SwitchingBusinessEntityAnnotation> findSwitchingBusinessEntityAnnotation(
-            final JsonStreamContext context) {
+    Optional<SwitchingBusinessEntityAnnotation> findSwitchingBusinessEntityAnnotation(final JsonStreamContext context) {
         Check.notNullArgument(context, "context");
-        
+
         Object currentValue = context.getCurrentValue();
-        
-        if(LOGGER.isDebugEnabled()) {
+
+        if (LOGGER.isDebugEnabled()) {
             LOGGER.debug("currentValue:" + currentValue + " " + (currentValue != null ? currentValue.getClass() : ""));
-        }        
+        }
 
         /*
          * check getter and fields for annotation before class, because they have higher
@@ -79,13 +93,15 @@ public class SwitchingAnnotationScanner {
 
             if (context.getCurrentName() != null) {
                 String jsonPropertyName = context.getCurrentName();
-                
-                Optional<SwitchingBusinessEntityAnnotation> fieldAnnotation = findAnnotationAtField(jsonPropertyName, currentClass);
+
+                Optional<SwitchingBusinessEntityAnnotation> fieldAnnotation = findAnnotationAtField(jsonPropertyName,
+                        currentClass);
                 if (fieldAnnotation.isPresent()) {
                     return fieldAnnotation;
                 }
-                
-                Optional<SwitchingBusinessEntityAnnotation> methodAnnotation = findAnnotationAtGetter(jsonPropertyName, currentClass);
+
+                Optional<SwitchingBusinessEntityAnnotation> methodAnnotation = findAnnotationAtGetter(jsonPropertyName,
+                        currentClass);
                 if (methodAnnotation.isPresent()) {
                     return methodAnnotation;
                 }
@@ -103,26 +119,42 @@ public class SwitchingAnnotationScanner {
 
         return Optional.empty();
     }
-   
-  
-    private Optional<SwitchingBusinessEntityAnnotation> findAnnotationAtField(final String jsonPropertyName, Class<? extends Object> currentClass) {
+
+    /**
+     * Try to find the {@link SwitchingBusinessEntityAnnotation} at a field named {@code jsonPropertyName} at the
+     * given class. 
+     *
+     * @param jsonPropertyName the json property name
+     * @param clazz the examined class
+     * @return the found {@link SwitchingBusinessEntityAnnotation}
+     */
+    private Optional<SwitchingBusinessEntityAnnotation> findAnnotationAtField(final String jsonPropertyName,
+            final Class<? extends Object> clazz) {
         Check.notEmptyArgument(jsonPropertyName, "jsonPropertyName");
-        Check.notNullArgument(currentClass, "currentClass");
+        Check.notNullArgument(clazz, "clazz");
 
         // TODO find field instead of use exception
         try {
 
-            Field field = currentClass.getDeclaredField(jsonPropertyName);
-            return Optional
-                    .ofNullable(field.getAnnotation(SwitchingBusinessEntityAnnotation.class));
+            Field field = clazz.getDeclaredField(jsonPropertyName);
+            return Optional.ofNullable(field.getAnnotation(SwitchingBusinessEntityAnnotation.class));
         } catch (NoSuchFieldException e) {
             return Optional.empty();
         }
     }
-    
-    private Optional<SwitchingBusinessEntityAnnotation> findAnnotationAtGetter(final String jsonPropertyName, Class<? extends Object> currentClass) {        
+
+    /**
+     * Try to find the {@link SwitchingBusinessEntityAnnotation} at a Getter named after {@code jsonPropertyName} at
+     * the given class.
+     *
+     * @param jsonPropertyName the json property name
+     * @param clazz the examined class
+     * @return the found {@link SwitchingBusinessEntityAnnotation}
+     */
+    private Optional<SwitchingBusinessEntityAnnotation> findAnnotationAtGetter(final String jsonPropertyName,
+            final Class<? extends Object> clazz) {
         Check.notEmptyArgument(jsonPropertyName, "jsonPropertyName");
-        Check.notNullArgument(currentClass, "currentClass");
+        Check.notNullArgument(clazz, "clazz");
 
         // TODO find method instead of use exception
         try {
@@ -130,19 +162,25 @@ public class SwitchingAnnotationScanner {
              * Getter are always public, so we can use getMethoth instead of
              * getDeclaredMethods and not not need a manual scan in super classes.
              */
-            Method method = currentClass.getMethod(getterName(jsonPropertyName));
-            return Optional
-                    .ofNullable(method.getAnnotation(SwitchingBusinessEntityAnnotation.class));
+            Method method = clazz.getMethod(getterName(jsonPropertyName));
+            return Optional.ofNullable(method.getAnnotation(SwitchingBusinessEntityAnnotation.class));
         } catch (NoSuchMethodException e) {
             return Optional.empty();
         }
     }
-    
-    private Optional<SwitchingBusinessEntityAnnotation> findAnnotationAtClass(Class<? extends Object> currentClass) {
-        Check.notNullArgument(currentClass, "currentClass");
+
+    /**
+     * Try to find the {@link SwitchingBusinessEntityAnnotation} at the class level.
+     *
+     * @param clazz the examined class
+     * @return the found {@link SwitchingBusinessEntityAnnotation}
+     */
+    private Optional<SwitchingBusinessEntityAnnotation> findAnnotationAtClass(
+            final Class<? extends Object> clazz) {
+        Check.notNullArgument(clazz, "clazz");
 
         Optional<SwitchingBusinessEntityAnnotation> classAnnotation = Optional
-                .ofNullable(currentClass.getAnnotation(SwitchingBusinessEntityAnnotation.class));
+                .ofNullable(clazz.getAnnotation(SwitchingBusinessEntityAnnotation.class));
         return classAnnotation;
     }
 
@@ -152,11 +190,10 @@ public class SwitchingAnnotationScanner {
      * @param fieldName the field name
      * @return the name of the Getter for the field.
      */
-    public static String getterName(final String fieldName) {
+    private static String getterName(final String fieldName) {
         Check.notNullArgument(fieldName, "fieldName");
 
-        //we do not need to check for isser-because there can not be a boolean in hits path
-        
+        /* we do not need to check for isser-because there can not be a boolean in hits path */
         return "get" + fieldName.substring(0, 1).toUpperCase(Locale.ENGLISH) + fieldName.substring(1);
     }
 }
