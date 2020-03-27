b7ba02dcf4ddd586908ede5462ceee7a66f73e7b
Ralph Engelmann <r.engelmann@queo-group.com>
2020-03-27 08:24:29 +0000

    Refactoring

--- queo-commons-persistence-extra-json/src/main/java/com/queomedia/persistence/extra/json/switchingbusinessentity/SwitchingAnnotationScanner.java
+++ queo-commons-persistence-extra-json/src/main/java/com/queomedia/persistence/extra/json/switchingbusinessentity/SwitchingAnnotationScanner.java
@@ -1,5 +1,9 @@
 package com.queomedia.persistence.extra.json.switchingbusinessentity;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
 import java.lang.reflect.Field;
 import java.lang.reflect.Method;
 import java.util.Locale;
@@ -11,7 +15,11 @@ import com.queomedia.commons.checks.Check;
 /**
  * Scan the json-context-path to find a class that is annotated with {@link SwitchingBusinessEntityAnnotation}.  
  */
-class SwitchingAnnotationScanner {
+public class SwitchingAnnotationScanner {
+    
+    /** Logger for this class. */
+    private static final Logger LOGGER = LoggerFactory.getLogger(SwitchingAnnotationScanner.class);
+
     
     /** The serialization mode returned if no annotation is found. */
     private final BusinessEntitySerializationMode defaultMode;
@@ -30,11 +38,23 @@ class SwitchingAnnotationScanner {
         return findSwitchingBusinessEntityAnnotation(context).map(SwitchingBusinessEntityAnnotation::value);
     }
 
+    /**
+     * Try to find the {@link SwitchingBusinessEntityAnnotation} in the given json {@code context}s
+     * {@link JsonStreamContext#getCurrentValue() context.getCurrentValue()} or if not found recursive in the
+     * {@link JsonStreamContext#getParent() context.getParent()}.
+     *
+     * @param context the {@link JsonStreamContext} thats currentValue and parentContext is scanned
+     * @return the first found {@link SwitchingBusinessEntityAnnotation}
+     */
     Optional<SwitchingBusinessEntityAnnotation> findSwitchingBusinessEntityAnnotation(
             final JsonStreamContext context) {
+        Check.notNullArgument(context, "context");
+        
         Object currentValue = context.getCurrentValue();
-        System.out.println(
-                "currentValue:" + currentValue + " " + (currentValue != null ? currentValue.getClass() : ""));
+        
+        if(LOGGER.isDebugEnabled()) {
+            LOGGER.debug("currentValue:" + currentValue + " " + (currentValue != null ? currentValue.getClass() : ""));
+        }        
 
         /*
          * check getter and fields for annotation before class, because they have higher
@@ -44,32 +64,15 @@ class SwitchingAnnotationScanner {
             Class<? extends Object> currentClass = currentValue.getClass();
 
             if (context.getCurrentName() != null) {
-                // TODO find field instead of use exception
-                try {
-
-                    Field field = currentClass.getDeclaredField(context.getCurrentName());
-                    Optional<SwitchingBusinessEntityAnnotation> fieldAnnotation = Optional
-                            .ofNullable(field.getAnnotation(SwitchingBusinessEntityAnnotation.class));
-                    if (fieldAnnotation.isPresent()) {
-                        return fieldAnnotation;
-                    }
-                } catch (NoSuchFieldException e) {
-                    // TODO scan super classes
+                                
+                Optional<SwitchingBusinessEntityAnnotation> fieldAnnotation = findAnnotationAtField(context, currentClass);
+                if (fieldAnnotation.isPresent()) {
+                    return fieldAnnotation;
                 }
-
-                // TODO find methos instead of use exception
-                try {
-                    /*
-                     * Getter are always public, so we can use getMethoth instead of
-                     * getDeclaredMethods and not not need a manual scan in super classes.
-                     */
-                    Method method = currentClass.getMethod(getterName(context.getCurrentName()));
-                    Optional<SwitchingBusinessEntityAnnotation> methodAnnotation = Optional
-                            .ofNullable(method.getAnnotation(SwitchingBusinessEntityAnnotation.class));
-                    if (methodAnnotation.isPresent()) {
-                        return methodAnnotation;
-                    }
-                } catch (NoSuchMethodException e) {
+                
+                Optional<SwitchingBusinessEntityAnnotation> methodAnnotation = findAnnotationAtGetter(context, currentClass);
+                if (methodAnnotation.isPresent()) {
+                    return methodAnnotation;
                 }
             }
             Optional<SwitchingBusinessEntityAnnotation> classAnnotation = Optional
@@ -85,6 +88,33 @@ class SwitchingAnnotationScanner {
         }
 
         return Optional.empty();
+    }
+  
+    private Optional<SwitchingBusinessEntityAnnotation> findAnnotationAtField(final JsonStreamContext context, Class<? extends Object> currentClass) {
+        // TODO find field instead of use exception
+        try {
+
+            Field field = currentClass.getDeclaredField(context.getCurrentName());
+            return Optional
+                    .ofNullable(field.getAnnotation(SwitchingBusinessEntityAnnotation.class));
+        } catch (NoSuchFieldException e) {
+            return Optional.empty();
+        }
+    }
+    
+    private Optional<SwitchingBusinessEntityAnnotation> findAnnotationAtGetter(final JsonStreamContext context, Class<? extends Object> currentClass) {
+        // TODO find methos instead of use exception
+        try {
+            /*
+             * Getter are always public, so we can use getMethoth instead of
+             * getDeclaredMethods and not not need a manual scan in super classes.
+             */
+            Method method = currentClass.getMethod(getterName(context.getCurrentName()));
+            return Optional
+                    .ofNullable(method.getAnnotation(SwitchingBusinessEntityAnnotation.class));
+        } catch (NoSuchMethodException e) {
+            return Optional.empty();
+        }
     }
 
     /**
