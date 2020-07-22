bb6e23957ce79bc2cf0093ca95f0b0d8cc8c772b
Ralph Engelmann <r.engelmann@queo-group.com>
2020-07-22 17:18:58 +0200

    add testcases for list serialization and deserialization

--- queo-commons-persistence-extra-json/src/test/java/com/queomedia/persistence/extra/json/BusinessEntityModuleTest.java
+++ queo-commons-persistence-extra-json/src/test/java/com/queomedia/persistence/extra/json/BusinessEntityModuleTest.java
@@ -3,7 +3,9 @@ package com.queomedia.persistence.extra.json;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.Assert.assertEquals;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import org.jmock.Expectations;
@@ -15,6 +17,7 @@ import org.skyscreamer.jsonassert.JSONAssert;
 import org.skyscreamer.jsonassert.JSONCompareMode;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JavaType;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.queomedia.persistence.BusinessEntity;
 import com.queomedia.persistence.BusinessId;
@@ -53,6 +56,12 @@ public class BusinessEntityModuleTest {
         public DemoBusinessEntity(final BusinessId<DemoBusinessEntity> businessId) {
             super(businessId);
         }
+
+        @Override
+        public String toString() {
+            return "DemoBusinessEntity []";
+        }
+
     }
 
     /**
@@ -114,15 +123,15 @@ public class BusinessEntityModuleTest {
             super();
         }
 
-        public MapEntityKeyContainer(Map<DemoBusinessEntity, Integer> map) {
+        public MapEntityKeyContainer(final Map<DemoBusinessEntity, Integer> map) {
             this.map = map;
         }
 
         public Map<DemoBusinessEntity, Integer> getMap() {
-            return map;
+            return this.map;
         }
 
-        public void setMap(Map<DemoBusinessEntity, Integer> map) {
+        public void setMap(final Map<DemoBusinessEntity, Integer> map) {
             this.map = map;
         }
 
@@ -144,7 +153,7 @@ public class BusinessEntityModuleTest {
 
         // @formatter:off
         final String expectedJson =
-                   "{'map': {        " 
+                   "{'map': {        "
                  + "  '123' : 1,     "
                  + "  '456' : 2      "
                  + "}}               ";
@@ -176,7 +185,7 @@ public class BusinessEntityModuleTest {
 
         // @formatter:off
         final String jsonString =(
-                   "{'map': {        " 
+                   "{'map': {        "
                  + "  '123' : 1,     "
                  + "  '456' : 2      "
                  + "}}               ").replaceAll("'", "\"");
@@ -199,6 +208,138 @@ public class BusinessEntityModuleTest {
         MapEntityKeyContainer result = mapper.readValue(jsonString, MapEntityKeyContainer.class);
 
         assertThat(result).usingRecursiveComparison().ignoringAllOverriddenEquals().isEqualTo(expectedMapContainer);
+    }
+
+    public static class ListEntityKeyContainer {
+        private List<DemoBusinessEntity> list = new ArrayList<>();
+
+        public ListEntityKeyContainer() {
+            super();
+        }
+
+        public ListEntityKeyContainer(final List<DemoBusinessEntity> list) {
+            this.list = list;
+        }
+
+        public List<DemoBusinessEntity> getList() {
+            return this.list;
+        }
+
+        public void setList(final List<DemoBusinessEntity> list) {
+            this.list = list;
+        }
+
+        @Override
+        public String toString() {
+            return "ListEntityKeyContainer [list=" + this.list + "]";
+        }
+
+    }
+
+    /**
+     * Scenario: serialize a Map where the keys are Business Entityies
+     *
+     * @throws Exception - no exception should not been thrown in this test case
+     */
+    @Test
+    public void testSerializerForCollection() throws Exception {
+        DemoBusinessEntity element1 = new DemoBusinessEntity(new BusinessId<>(123));
+        DemoBusinessEntity element2 = new DemoBusinessEntity(new BusinessId<>(456));
+        List<DemoBusinessEntity> list = new ArrayList<>();
+        list.add(element1);
+        list.add(element2);
+
+        ListEntityKeyContainer listContainer = new ListEntityKeyContainer(list);
+
+        // @formatter:off
+        final String expectedJson =
+                   "{'list': ['123', '456']}";
+        // @formatter:on
+
+        /* when: using jackson with the BussinessEntiyModule to serialize the list of Business entity */
+        final GeneralLoaderDao generalLoaderDao = this.context.mock(GeneralLoaderDao.class);
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.registerModule(new BusinessEntityModule(generalLoaderDao));
+
+        String jsonResult = mapper.writeValueAsString(listContainer);
+        System.out.println(jsonResult);
+
+        JSONAssert.assertEquals(expectedJson, jsonResult, JSONCompareMode.NON_EXTENSIBLE);
+    }
+
+    /**
+     * Scenario: serialize a Map where the keys are Business Entityies
+     *
+     * @throws Exception - no exception should not been thrown in this test case
+     */
+    @Test
+    public void testDeserializerForCollection() throws Exception {
+        DemoBusinessEntity element1 = new DemoBusinessEntity(new BusinessId<>(123));
+        DemoBusinessEntity element2 = new DemoBusinessEntity(new BusinessId<>(456));
+        List<DemoBusinessEntity> list = new ArrayList<>();
+        list.add(element1);
+        list.add(element2);
+
+        ListEntityKeyContainer expectedListContainer = new ListEntityKeyContainer(list);
+
+        // @formatter:off
+        final String jsonString =
+                   "{'list': ['123', '456']}".replace('\'', '"');
+        // @formatter:on
+
+        final GeneralLoaderDao generalLoaderDao = this.context.mock(GeneralLoaderDao.class);
+        ObjectMapper mapper = new ObjectMapper();
+        this.context.checking(new Expectations() {
+            {
+                oneOf(generalLoaderDao).getByBusinessId(element1.getBusinessId(), DemoBusinessEntity.class);
+                will(returnValue(element1));
+
+                oneOf(generalLoaderDao).getByBusinessId(element2.getBusinessId(), DemoBusinessEntity.class);
+                will(returnValue(element2));
+            }
+        });
+        mapper.registerModule(new BusinessEntityModule(generalLoaderDao));
+
+        /* when: using jackson with the BussinessEntiyModule to deserialize to a list of Business entity */
+        ListEntityKeyContainer result = mapper.readValue(jsonString, ListEntityKeyContainer.class);
+        assertThat(result).usingRecursiveComparison().ignoringAllOverriddenEquals().isEqualTo(expectedListContainer);
+    }
+
+    /**
+     * Scenario: serialize a Map where the keys are Business Entityies
+     *
+     * @throws Exception - no exception should not been thrown in this test case
+     */
+    @Test
+    public void testDeserializerForCollectionWithoutContainer() throws Exception {
+        DemoBusinessEntity element1 = new DemoBusinessEntity(new BusinessId<>(123));
+        DemoBusinessEntity element2 = new DemoBusinessEntity(new BusinessId<>(456));
+
+        // @formatter:off
+        final String jsonString =
+                   "['123', '456']".replace('\'', '"');
+        // @formatter:on
+
+        final GeneralLoaderDao generalLoaderDao = this.context.mock(GeneralLoaderDao.class);
+        ObjectMapper mapper = new ObjectMapper();
+        this.context.checking(new Expectations() {
+            {
+                oneOf(generalLoaderDao).getByBusinessId(element1.getBusinessId(), DemoBusinessEntity.class);
+                will(returnValue(element1));
+
+                oneOf(generalLoaderDao).getByBusinessId(element2.getBusinessId(), DemoBusinessEntity.class);
+                will(returnValue(element2));
+            }
+        });
+        mapper.registerModule(new BusinessEntityModule(generalLoaderDao));
+
+        /* when: using jackson with the BussinessEntiyModule to deserialize to a list of Business entity */
+
+        JavaType deserializationType = mapper.getTypeFactory().constructParametricType(List.class,
+                DemoBusinessEntity.class);
+        List<DemoBusinessEntity> result = mapper.readValue(jsonString, deserializationType);
+
+        assertThat(result).containsExactly(element1, element2);
     }
 
 }
